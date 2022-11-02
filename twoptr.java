7962b7a3ccfee448a882f308078503a4ab912177
Lingjie Tao <30500113+LuckyJanine@users.noreply.github.com>
2022-11-02 23:54:14 +0100

    findMaxConsecutiveOnes

--- twoptr.java
+++ twoptr.java
@@ -1,6 +1,8 @@
 package Array_String;
 
 public class twoptr {
+    
+    // input: an array of chars
     public void reverseString(char[] s) {
         if(s.length == 0|| s.length == 1){
             System.out.println("there is nothing to swap");
@@ -14,5 +16,22 @@ public class twoptr {
             i++;
             j--;
         }
+    }
+    
+    // input: an array of binary numbers
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxNumberOfOnes =0;
+        int counter = 0;
+        for(int i =0; i < nums.length; i++){
+            if(nums[i] == 1){
+                counter = counter +1;
+                if(counter > maxNumberOfOnes){
+                    maxNumberOfOnes = counter;
+                }
+            } else {
+                counter = 0;
+            }
+        }
+        return maxNumberOfOnes;
     }
 }
