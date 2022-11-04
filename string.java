fc03ae3d449a1d5008dd29f4ca5bf6533beced76
Lingjie Tao <30500113+LuckyJanine@users.noreply.github.com>
2022-11-04 21:04:23 +0100

    Add files via upload

---
+++ string.java
@@ -0,0 +1,25 @@
+package Array_String;
+
+public class string {
+
+    public int lengthOfLastWord(String s) {
+        int len = s.length();
+        int lenOfLastWord = 0;
+        int i = len -1;
+        // remove the lagging spaces
+        while(i >= 0){
+            if(s.charAt(i) != ' '){
+                break;
+            }
+            i--;
+        }
+        // start counting
+        for(int j = i; j >= 0; j--){
+            if(s.charAt(j) == ' '){
+                break;
+            }
+            lenOfLastWord++;
+        }
+        return lenOfLastWord;
+    }
+}
